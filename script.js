80ffc5b7a999e74ec820a5f439b27f649fc2e343
Zena-art <zena.nazim@gmail.com>
2024-09-12 13:04:51 -0500

    declared variables

---
+++ script.js
@@ -0,0 +1,23 @@
+//Part 1: Growing Pains
+
+// declaring variables
+
+const PI = 3.1415;
+const radius = 5;
+const area = PI * radius * radius;
+// plant doubles every week
+let growthWeek = 2;
+let plantCount = 20;
+let plantSize = 0.8;
+
+// how many plants we can have 
+let maxCapacity = area / plantSize;
+
+//Predict the plant growth after a specific number of weeks.
+
+const week = plantSize *  plantCount;
+if ((week / maxCapacity) * 100 <= 50){
+  console.log("Planted");
+}else if((week / maxCapacity) * 100 >= 50 && (week / maxCapacity) * 100 <= 80){
+  console.log("Monitored");
+} 
