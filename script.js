d37ecd0690011d0dbac5d3bed4a6690288f99450
Zena-art <zena.nazim@gmail.com>
2024-09-13 10:59:51 -0500

    UpATE

--- script.js
+++ script.js
@@ -1,23 +1,113 @@
-//Part 1: Growing Pains
+// Part 1
 
-// declaring variables
-
-const PI = 3.1415;
 const radius = 5;
+const PI = 3.1415;
 const area = PI * radius * radius;
-// plant doubles every week
-let growthWeek = 2;
-let plantCount = 20;
-let plantSize = 0.8;
 
-// how many plants we can have 
-let maxCapacity = area / plantSize;
+const initialPlants = 20;
+const minimumSpace = 0.8;
 
-//Predict the plant growth after a specific number of weeks.
+let currentWeek = 0;
 
-const week1 = plantSize *  plantCount;
-if ((week1 / maxCapacity) * 100 <= 50){
-  console.log("Planted");
-}else if((week1 / maxCapacity) * 100 >= 50 && (week / maxCapacity) * 100 <= 80){
-  console.log("Monitored");
+let plants = initialPlants * 2 ** currentWeek;
+
+let plantSpace = plants * 0.8;
+
+const gardern80Percent = area * 0.8;
+const gardern50Percent = area * 0.5;
+
+//Predictions
+
+console.log('============== Week 1 =================');
+// Week 1
+// ==============================================
+console.log(`Total Plants: ${plants}
+Space needed for plants ${plantSpace} square meters
+Garden Area: ${area}`);
+
+if (plantSpace > gardern80Percent) {
+  console.log(
+    `Week: ${
+      currentWeek + 1
+    } | Prune: stop them from exceeding the capacity of the garden`
+  );
+} else if (plantSpace > gardern50Percent) {
+  console.log(
+    `Week: ${
+      currentWeek + 1
+    } | Monitor: they are growing at an acceptable rate.`
+  );
+} else {
+  console.log(
+    `Week: ${currentWeek + 1} | Plant: there is room to plant more plants`
+  );
 }
+//===============================================
+console.log('============== Week 2 =================');
+// Week 2
+// ==============================================
+
+// increment the week
+currentWeek++;
+
+// recalculate plant stats
+plants = initialPlants * 2 ** currentWeek;
+plantSpace = plants * 0.8;
+
+console.log(`Total Plants: ${plants}
+  Space needed for plants ${plantSpace} square meters
+  Garden Area: ${area}`);
+
+if (plantSpace > gardern80Percent) {
+  console.log(
+    `Week: ${
+      currentWeek + 1
+    } | Prune: stop them from exceeding the capacity of the garden`
+  );
+} else if (plantSpace > gardern50Percent) {
+  console.log(
+    `Week: ${
+      currentWeek + 1
+    } | Monitor: they are growing at an acceptable rate.`
+  );
+} else {
+  console.log(
+    `Week: ${currentWeek + 1} | Plant: there is room to plant more plants`
+  );
+}
+//===============================================
+
+// Week 3
+console.log('============== Week 3 =================');
+// ==============================================
+
+// increment the week
+currentWeek++;
+
+// recalculate plant stats
+plants = initialPlants * 2 ** currentWeek;
+
+plantSpace = plants * 0.8;
+
+console.log(`Total Plants: ${plants}
+  Space needed for plants ${plantSpace} square meters
+  Garden Area: ${area}`);
+
+if (plantSpace > gardern80Percent) {
+  console.log(
+    `Week: ${
+      currentWeek + 1
+    } | Prune: stop them from exceeding the capacity of the garden`
+  );
+} else if (plantSpace > gardern50Percent) {
+  console.log(
+    `Week: ${
+      currentWeek + 1
+    } | Monitor: they are growing at an acceptable rate.`
+  );
+} else {
+  console.log(
+    `Week: ${currentWeek + 1} | Plant: there is room to plant more plants`
+  );
+}
+//===============================================
